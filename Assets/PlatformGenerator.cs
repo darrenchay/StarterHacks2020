e9881b95f87ca3ac1a330b37307decf24202a9ea
darrenchay <darrenchay@gmail.com>
2020-01-19 03:39:43 -0500

    added more stuff

--- Assets/PlatformGenerator.cs
+++ Assets/PlatformGenerator.cs
@@ -7,23 +7,26 @@ public class PlatformGenerator : MonoBehaviour {
     public GameObject thePlatform;
     public Transform generationPoint;
     public float distanceBetween;
-
+    public float targetTime = 0.0f;
     private float platformWidth;
+    Random random;
 
     // Start is called before the first frame update
     void Start()
     {
-        platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x; 
+        platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
+        random = new Random();
     }
 
     // Update is called once per frame
-    void Update() { 
-        
-        if(transform.position.x < generationPoint.position.x)
+    void Update() {
+        targetTime -= Time.deltaTime;
+        if (targetTime <= 0.0f)
         {
-            transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
+            transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y + Random.Range(-3,3), transform.position.z);
 
           Instantiate(thePlatform, transform.position, transform.rotation);
+            targetTime = 1.0f;
         }
 
     }
